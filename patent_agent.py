cfe8b4998810ddadd221fcc14e04b4e26ef33b04
Souparna08 <souparnamaji0802@gmail.com>
2025-12-27 18:31:06 +0530

    patent_agent.py

--- patent_agent.py
+++ patent_agent.py
@@ -2,24 +2,21 @@ from rdkit import Chem
 from rdkit.Chem import AllChem, DataStructs
 import pandas as pd
 
-# ---------------------------
+
 # Load Patent Database
-# ---------------------------
+
 def load_patent_db(path):
     return pd.read_csv(path)
 
-# ---------------------------
-# Compute Molecular Fingerprint
-# ---------------------------
 def get_fingerprint(smiles):
     mol = Chem.MolFromSmiles(smiles)
     if mol is None:
         return None
     return AllChem.GetMorganFingerprintAsBitVect(mol, 2, 2048)
 
-# ---------------------------
+
 # Compare Query Molecule With Patent DB
-# ---------------------------
+
 def find_similar_patent(query_smiles, df):
     query_fp = get_fingerprint(query_smiles)
 
@@ -40,7 +37,7 @@ def find_similar_patent(query_smiles, df):
             best_score = score
             best_match = row
 
-    if best_score < 0.4:     # threshold
+    if best_score < 0.4:   
         return None
 
     result = best_match.to_dict()
